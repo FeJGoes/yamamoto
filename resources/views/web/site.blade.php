0589edd240b62f2f98e90f420dd14a243bedd23d
Felipe J. de Góes <felipe.92goes@gmail.com>
2020-09-26 21:45:25 -0300

    bootstrap da página inicial

--- resources/views/web/site.blade.php
+++ resources/views/web/site.blade.php
@@ -1,16 +1,21 @@
 <!DOCTYPE html>
 <html lang="pt-BR">
-    <head>
-        @include('web.layout.head')
-    </head>
+    @include('web.layout.head')
 
     <body>
+        @include('web.layout.header')
 
-        <main class="container">
+        <main role="main" class="container">
             @yield('content')
         </main>
 
         @include('web.layout.footer')
+
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+
+        @stack('scripts')
     </body>
 </html>
 
