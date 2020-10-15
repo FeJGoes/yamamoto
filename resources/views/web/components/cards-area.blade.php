a4702e3125a6cdb7d9dc8da0905a22107dad6e19
Felipe J. de Góes <felipe.92goes@gmail.com>
2020-10-14 22:19:43 -0300

    cards videos component feito

--- resources/views/web/components/cards-area.blade.php
+++ resources/views/web/components/cards-area.blade.php
@@ -1,38 +1,43 @@
 <section id="ty-cards-area">
+
     <div class="uk-card uk-card-default ty-card">
         <div class="uk-card-body">
-            <figure>
+            <a href="#" target="_blank">
                 <img src="{{asset('images/IMG_1269.jpg')}}" alt="thumbnail">
-            </figure>
+            </a>
             <h3>Titulo muito bom</h3>
             <p>Mussum Ipsum, cacilds vidis litro abertis. Mauris nec dolor in eros commodo tempor.</p>
         </div>
     </div>
+
     <div class="uk-card uk-card-default ty-card">
         <div class="uk-card-body">
-            <figure>
+            <a href="#" target="_blank">
                 <img src="{{asset('images/IMG_1269.jpg')}}" alt="thumbnail">
-            </figure>
+            </a>
             <h3>Titulo muito bom</h3>
             <p>Mussum Ipsum, cacilds vidis litro abertis. Mauris nec dolor in eros commodo tempor.</p>
         </div>
     </div>
+
     <div class="uk-card uk-card-default ty-card">
         <div class="uk-card-body">
-            <figure>
+            <a href="#" target="_blank">
                 <img src="{{asset('images/IMG_1269.jpg')}}" alt="thumbnail">
-            </figure>
+            </a>
             <h3>Titulo muito bom</h3>
             <p>Mussum Ipsum, cacilds vidis litro abertis. Mauris nec dolor in eros commodo tempor.</p>
         </div>
     </div>
+
     <div class="uk-card uk-card-default ty-card">
         <div class="uk-card-body">
-            <figure>
+            <a href="#" target="_blank">
                 <img src="{{asset('images/IMG_1269.jpg')}}" alt="thumbnail">
-            </figure>
+            </a>
             <h3>Titulo muito bom</h3>
             <p>Mussum Ipsum, cacilds vidis litro abertis. Mauris nec dolor in eros commodo tempor.</p>
         </div>
     </div>
+
 </section>
