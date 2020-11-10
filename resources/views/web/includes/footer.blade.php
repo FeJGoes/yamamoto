502bf7979b96568dbfd3340c6375011be22b272e
Felipe J. de Góes <felipe.92goes@gmail.com>
2020-11-09 22:26:35 -0300

    ajuste resumo mandato e footer biografia

--- resources/views/web/includes/footer.blade.php
+++ resources/views/web/includes/footer.blade.php
@@ -3,22 +3,21 @@
     <div class="uk-width-1-4@m uk-visible@m">
       <article>
         <h3 class="ty-tc3">Thiago Yamamoto</h3>
-        <p>Thiago Yamamoto, descendente de Japoneses, casado há 9 anos com Anna Paula Teixeira Yamamoto, morador do
-          Grande Cervezão (Jardim São joão, Parque das Industrias e jardim Independência), viveu 10 anos no Parque São
-          Jorge e atualmente reside no Jardim Independência. Graduado em gestão de produção industrial, pela faculdade
-          Uninter, atualmente está na segunda graduação em Gestão Pública também pela Uninter</p>
-        <p>Católico, atuou servindo a Deus durante 19 anos na RCC e atualmente continua servindo a Deus nas missões e
-          associação católica Maria, exerce também o ministério de pregação e música.</p>
+        <p><strong>Thiago Yamamoto</strong>, descendente de Japoneses, casado há 9 anos com Anna Paula Teixeira Yamamoto,
+          morador do Grande Cervezão (Jardim São joão, Parque das Industrias e jardim Independência), viveu 10 anos no
+          Parque São Jorge e atualmente reside no Jardim Independência. Graduado em gestão de produção industrial, pela
+          faculdade Uninter, atualmente está na segunda graduação em Gestão Pública também pela Uninter</p>
+        <p>Católico, atuou servindo a Deus durante 19 anos na Renovação Carismática Católica (RCC) e atualmente continua servindo a Deus nas Missões e Associação Católica Maria Peregrina, exerce também o ministério de pregação e música.</p>
         <p>Iniciou sua trajetória política nas eleições de 2016, eleito vereador da Cidade de Rio Claro com 934 votos.
-          Acredita que a política é o lugar do diálogo e o caminho para busca do bem comum. Defensor de uma gestão
-          pública inteligente e inovadora, suas principais atuações são voltadas à área da educação, da saúde, da defesa
-          da vida e da família.</p>
+          Acredita que a política é o lugar do diálogo e o caminho para busca do bem comum. Defensor de uma gestão pública
+          inteligente e inovadora, suas principais atuações são voltadas à área da educação, da saúde, da defesa da vida e
+          da família.</p>
       </article>
     </div>
 
     <div class="uk-width-1-4@m ty-resume">
       <h4 class="ty-tc3">Resumo do mandato</h4>
-      <a href="{{asset('archive/resumo-mandato.pdf')}}" target="_blank" download>
+      <a href="{{asset('archive/resumo-mandato-v2.pdf')}}" target="_blank" download>
         <img src="{{asset('images/cartilha.webp')}}" alt="cartilha">
       </a>
     </div>
